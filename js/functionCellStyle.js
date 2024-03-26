a80f3acd2e9e1684606496926f1b1deb18e2cbca
XJ581G <164125522+XJ581G@users.noreply.github.com>
2024-03-25 18:11:12 -0600

    Update functionCellStyle.js

--- js/functionCellStyle.js
+++ js/functionCellStyle.js
@@ -1,41 +1,10 @@
-function cellStyle(value, row, index, field) {
-  if (field == 'text2') {
-    return {classes: 'warning'};
-  }
-  else {
+function cellStyle(value, row, index) {
+    var classes = ['active', 'success', 'info', 'warning', 'danger'];
+    
+    if (index % 2 === 0 && index / 2 < classes.length) {
+        return {
+            classes: classes[index / 2]
+        };
+    }
     return {};
-  }
 }
-
-
-var data = [{"text1": "text1", "text2": "text2"}];
-
-$(function () {
-  $('#table').bootstrapTable({
-    columns: [
-      {
-        field: 'text1',
-        title: 'TEXT 1',
-      },
-      {
-        field: 'text2',
-        title: 'TEXT 2',
-        cellStyle: cellStyle,
-      }
-    ],
-    data: data
-  });
-});
-
-var $table = $('#table'),
-    $button = $('#button');
-$(function () {
-  $button.click(function () {
-    $table.bootstrapTable('insertRow', {
-      index: 0,
-      row: {
-        text2: 'new text'
-      }
-    });
-  });
-});
